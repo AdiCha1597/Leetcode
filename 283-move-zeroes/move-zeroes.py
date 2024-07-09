f18bb06d9dedb3c24f449138a12214d86e5f2b19
AdiCha1597 <63296940+AdiCha1597@users.noreply.github.com>
2024-07-08 18:51:18 -0700

    Time: 121 ms (80.50%) | Memory: 18.3 MB (16.01%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,12 +6,10 @@ class Solution:
         l, r = 0, 1
         while r < len(nums):
             if nums[l] == 0 and nums[r] != 0:
-                print('case1')
                 nums[l], nums[r] = nums[r], nums[l]
                 l +=1
                 r +=1
             elif nums[l] == 0 and nums[r] == 0:
-                print('case2')
                 r +=1
             elif nums[l] != 0 and nums[r] != 0:
                 l +=1
