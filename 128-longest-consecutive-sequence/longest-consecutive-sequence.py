801a68c6ca12a3ec72d6abe1cc317c4f47832910
AdiCha1597 <63296940+AdiCha1597@users.noreply.github.com>
2024-07-17 00:58:46 -0700

    Time: 4478 ms (17.08%) | Memory: 31.8 MB (64.03%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -0,0 +1,63 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # Convert the list to a set to remove duplicates and allow O(1) look-up times
+        nums_set = set(nums)
+        maxVal = 0  # Initialize the maximum length of consecutive sequence
+        
+        for n in nums:
+            cnt = 1  # Start counting with the current number
+            curr = n + 1  # Next number to look for in the sequence
+            # Only start counting if 'n' is the beginning of a sequence
+            if (n - 1) not in nums_set:
+                # Increment the sequence length if the next number is in the set
+                while curr in nums_set:
+                    cnt += 1
+                    curr += 1
+                # Update the maximum length found so far
+                maxVal = max(maxVal, cnt)
+        return maxVal
+        # TC :O(n) SC: O(n)
+
+
+
+
+
+
+
+
+
+        #[100,4,200,1,3,2]
+        # hmap = {}
+        
+        # for num in nums:
+        #     if num - 1 in hmap:
+        #         hmap[num-1] = num
+        #     if num + 1 in hmap:
+        #         hmap[num] = num+1
+        #     if num not in hmap:
+        #         hmap[num] = float(inf)
+        
+        # maxVal = 0
+        # for k,v in hmap.items():
+        #     prev = k
+        #     nxt = v
+        #     cnt = 1
+
+        #     while nxt in hmap:
+        #         cnt += 1
+        #         prev, nxt = nxt, hmap[nxt]
+        #     maxVal= max(maxVal, cnt)
+        # return maxVal
+
+
+        
+
+
+
+
+
+
+
+
+        
+            
