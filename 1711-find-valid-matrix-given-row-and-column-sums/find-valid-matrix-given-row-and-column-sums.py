1ecbebafdd739cccd7ce5fa5d9b9060e0871e2db
AdiCha1597 <63296940+AdiCha1597@users.noreply.github.com>
2024-07-20 16:01:55 -0700

    Time: 594 ms (22.57%) | Memory: 21.8 MB (35.76%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -1,15 +1,23 @@
 class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
         r, c = len(rowSum), len(colSum)
+        # initialize the matrix with 0's
         res = [[0 for _ in range(c)] for _ in range(r)]
         
         for i in range(r):
             for j in range(c):
+                #put the min value at res[i][j]
                 candidate = min(rowSum[i],colSum[j])
                 res[i][j] = candidate
+                # subtract the min value from rowSum and colSum
                 rowSum[i] -= candidate
                 colSum[j] -= candidate
         return res
+
+"""
+Time Complexity: O(r * c)
+Space Complexity: O(r *c)
+"""
                 
         
 
