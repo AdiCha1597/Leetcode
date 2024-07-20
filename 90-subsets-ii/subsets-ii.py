2fb9c3097d5f09ec597b02763c8bf58dbc1fef2f
AdiCha1597 <63296940+AdiCha1597@users.noreply.github.com>
2024-07-19 19:53:11 -0700

    Time: 42 ms (38.57%) | Memory: 16.8 MB (11.56%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,22 +1,30 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-
         res = []
         subset = []
+        # sort the nums, to get all the duplicate values next to each other.
         nums = sorted(nums)
 
-        def dfs(i):
-            if i >= len(nums):
-                if subset not in res:
-                    res.append(subset.copy())
+        #helper function to process the elements
+        def helper(i):
+            # base case: if subset is equal to len(nums), append the copy
+            if i == len(nums):
+                res.append(subset[::])
                 return 
+            # else append the element in the subset
             subset.append(nums[i])
-            dfs(i + 1)
-
+            #recursively call the function incrementing i
+            helper(i + 1)
+            #pop from the subset and call once the value is not same while
+            # incrementing.
             subset.pop()
-            dfs(i + 1)
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            # call the helper function once you get the next non-duplicate.
+            helper(i + 1)
         
-        dfs(0)
+        helper(0)
         return res
                 
                 
